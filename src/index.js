4babfc43b86cb27b560b3d082f808622561c4667
harithavishva <harithasai01@gmail.com>
2023-03-10 05:17:57 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+import Card from "../src/components/card";
+import services from "../src/components/services"
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// inserting props from service file and later using map method to loop over it in renderDom 
+
+function elements(items){
+  return( <Card
+  key={items.cost}
+    plan = {items.plan}
+    cost={items.cost}
+    duration={items.duration}
+    service1={items.service1}
+    service2={items.service2}
+    service3={items.service3}
+    service4={items.service4}
+    service5={items.service5}
+    service6={items.service6}
+    service7={items.service7}
+    service8={items.service8}/>)
+}
+
+ReactDOM.render(
+<div className="parentDiv"> 
+
+{services.map(elements)}
+</div>,
+document.getElementById("root"));
