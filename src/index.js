37628e1caef611bd1ebdbe018bc668be40f9758a
harithavishva <harithasai01@gmail.com>
2023-03-13 15:14:43 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -1,9 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 
-import App from './components/App'
+import Card from "../src/components/card";
+import services from "../src/components/services"
 
- ReactDOM.render(
-     <App/> ,
-     document.getElementById("root")
- )
+// inserting props from service file and later using map method to loop over it in renderDom 
+
+function elements(items){
+  return( <Card
+  key={items.cost}
+    plan = {items.plan}
+    cost={items.cost}
+    duration={items.duration}
+    service1={items.service1}
+    service2={items.service2}
+    service3={items.service3}
+    service4={items.service4}
+    service5={items.service5}
+    service6={items.service6}
+    service7={items.service7}
+    service8={items.service8}/>)
+}
+
+ReactDOM.render(
+<div className="parentDiv"> 
+
+{services.map(elements)}
+</div>,
+document.getElementById("root"));
